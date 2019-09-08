f769fe5221dd1930bb4a21c4c8b2dbe051794d1b
braden coates <braden@pop-os.localdomain>
2019-09-07 21:40:41 -0400

    almost done level 4

--- 001/level_4/level_4.ino
+++ 001/level_4/level_4.ino
@@ -1,11 +1,48 @@
-#define SW1 
+// defining pins
+#define SW1 2
+#define SW2 3
+#define rLED 12
+#define bLED 13
+
+// state variables
+bool on = false;
+bool switcher = true;
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(SW1, INPUT);
+  pinMode(SW2, INPUT);
+  pinMode(rLED, OUTPUT);
+  pinMode(bLED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  if (digitalRead(SW1) == HIGH) {
+    on = true;
+  }
+
+  while (on) {
+    
+    if (switcher) {
+      digitalWrite(rLED, HIGH);
+      digitalWrite(bLED, LOW);
+      switcher = false;
+    } else {
+      digitalWrite(rLED, HIGH);
+      digitalWrite(bLED, LOW);
+      switcher = true;
+    }
+
+    if (digitalRead(SW2) == HIGH) {
+      digitalWrite(rLED, LOW);
+      digitalWrite(bLED, LOW);
+      on = false;
+    }
+
+    delay(500);
+    
+  }
+  
 }
